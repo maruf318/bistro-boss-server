8c244bc117726091a0396f35044b3df64998255b
Maruf Hossain <hossainmaruf001@gmail.com>
2023-11-16 03:32:24 +0100

    patch method added to add admin role and delete method added to delete a user

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
     const usersCollection = client.db("bistroDb").collection("users");
 
     //users related api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user doesnt exists
@@ -45,6 +50,26 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
